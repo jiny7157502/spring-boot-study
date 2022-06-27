d5f7887a5c7bc3d4d88bdbcbe588c5260f7586eb
LAPTOP-8GDS7GCB\jiny7 <jiny7157502@naver.com>
2022-06-27 21:54:08 +0900

    Code Edit/Update User Function Commit

---
+++ MyWebApp/src/main/java/com/mystudy/user/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mystudy.user;
+
+public class UserNotFoundException extends Throwable {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
--- MyWebApp/src/main/java/com/mystudy/user/UserRepository.java
+++ MyWebApp/src/main/java/com/mystudy/user/UserRepository.java
@@ -3,4 +3,5 @@ package com.mystudy.user;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+    public Long countById(Integer id);
 }
